932d928465893d093405e7d0dd67a0e79fb9feb1
Justin Warner <Justin Warner>
2017-05-11 15:15:40 -0700

    no comment

---
+++ mother_of_all_demos/capacitive.py
@@ -0,0 +1,17 @@
+import sys, time
+from keyboardserial import KeyboardSerial
+
+ks = KeyboardSerial()
+
+print("Connecting to serial port on " + sys.argv[1])
+ks.connect(sys.argv[1])
+if ks.is_connected():
+    print("Successfully connected")
+		
+while True:
+	data = ks.get_sensor_data()
+	# DO DATA PROCESSING
+	ks.update_leds({'a': data[0], 's': data[1], 'd': data[2], 'f': data[3], 'j': data[4], 'k': data[5], 'l': data[6], ';': data[7]})
+	time.sleep(0.05)
+	
+ks.disconnect()
